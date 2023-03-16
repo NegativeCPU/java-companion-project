33d57291fb5f54d5f68788976506ab04305f6956
joseph.d.thomas <joseph.d.thomas@AFSWWVFWRTIT8J.us.accenturefederal.com>
2023-03-16 14:04:03 -0500

    Renamed interface to follow java naming conventions

---
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/GameService.java
@@ -0,0 +1,13 @@
+package com.organization.mvcproject.MGL_Task1.service;
+
+import java.util.List;
+
+import com.organization.mvcproject.model.Game;
+
+public interface GameService {
+
+	List<Game> retrieveAllGames();
+
+	Game saveGame(Game game);
+
+}
